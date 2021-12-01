7506df7ad20f7aed30d386f601d97cea413bbe37
Oscar Möller <oscarmoller94@outlook.com>
2021-12-01 15:48:19 +0100

    Added login template

--- WpfApp1/LoggInWindow.xaml.cs
+++ WpfApp1/LoggInWindow.xaml.cs
@@ -23,5 +23,14 @@ namespace WpfApp1
         {
             InitializeComponent();
         }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+        
     }
 }
