955ae4ff94cf4e57a2ab6913ece804f212c84981
Emma-LinLin <74680845+Emma-LinLin@users.noreply.github.com>
2021-12-01 13:10:28 +0100

    removed tests

--- BudgetCalculatorTests1/BudgetReportTests.cs
+++ BudgetCalculatorTests1/BudgetReportTests.cs
@@ -8,49 +8,49 @@ namespace BudgetCalculator.Tests
     [TestClass()]
     public class BudgetReportTests
     {
-        private TestSeeder seeder;
+        //private TestSeeder seeder;
 
-        [TestInitialize]
-        public void SetUp()
-        {
-            seeder = new TestSeeder();
-        }
+        //[TestInitialize]
+        //public void SetUp()
+        //{
+        //    seeder = new TestSeeder();
+        //}
 
-        [TestMethod()]
-        public void GetCalculatedDataToStringTest_SendInEcoController_ReturnsCollectedDataAsString()
-        {
-            seeder.InitList();
-            BudgetReport report = new(seeder.ecoController);
-            var actual = report.GetCalculatedDataToString().Trim();
-            WriteToFile writer = new();
+        //[TestMethod()]
+        //public void GetCalculatedDataToStringTest_SendInEcoController_ReturnsCollectedDataAsString()
+        //{
+        //    seeder.InitList();
+        //    BudgetReport report = new(seeder.ecoController);
+        //    var actual = report.GetCalculatedDataToString().Trim();
+        //    WriteToFile writer = new();
 
-            //In case file already exists--
-            writer.WriteStringToFile("test file", report.GetCalculatedDataToString());
-            File.Delete(writer.PathAndFileName);
-            //--
+        //    //In case file already exists--
+        //    writer.WriteStringToFile("test file", report.GetCalculatedDataToString());
+        //    File.Delete(writer.PathAndFileName);
+        //    //--
 
-            writer.WriteStringToFile("test file", report.GetCalculatedDataToString());
-            string expected = File.ReadAllText(writer.PathAndFileName).Trim();
+        //    writer.WriteStringToFile("test file", report.GetCalculatedDataToString());
+        //    string expected = File.ReadAllText(writer.PathAndFileName).Trim();
 
-            Assert.AreEqual(expected, actual);
-        }
+        //    Assert.AreEqual(expected, actual);
+        //}
 
-        [TestMethod()]
-        public void GetCalculatedDataToStringTest_Null_ReturnsEmptyString()
-        {           
-            BudgetReport report = new(null);
-            var actual = report.GetCalculatedDataToString().Trim();
-            WriteToFile writer = new();
+        //[TestMethod()]
+        //public void GetCalculatedDataToStringTest_Null_ReturnsEmptyString()
+        //{           
+        //    BudgetReport report = new(null);
+        //    var actual = report.GetCalculatedDataToString().Trim();
+        //    WriteToFile writer = new();
 
-            //In case file already exists--
-            writer.WriteStringToFile("test file", report.GetCalculatedDataToString());
-            File.Delete(writer.PathAndFileName);
-            //--
+        //    //In case file already exists--
+        //    writer.WriteStringToFile("test file", report.GetCalculatedDataToString());
+        //    File.Delete(writer.PathAndFileName);
+        //    //--
 
-            writer.WriteStringToFile("test file", report.GetCalculatedDataToString());
-            string expected = File.ReadAllText(writer.PathAndFileName).Trim();
+        //    writer.WriteStringToFile("test file", report.GetCalculatedDataToString());
+        //    string expected = File.ReadAllText(writer.PathAndFileName).Trim();
 
-            Assert.AreEqual(expected, actual);
-        }
+        //    Assert.AreEqual(expected, actual);
+        //}
     }
 }
--- BudgetCalculatorTests1/Helpers/ErrorLoggerTests.cs
+++ BudgetCalculatorTests1/Helpers/ErrorLoggerTests.cs
@@ -7,47 +7,47 @@ namespace BudgetCalculator.Helpers.Tests
     [TestClass()]
     public class ErrorLoggerTests
     {
-        private TestSeeder seeder;
+        //private TestSeeder seeder;
 
-        [TestInitialize]
-        public void SetUp()
-        {
-            seeder = new TestSeeder();
-        }
+        //[TestInitialize]
+        //public void SetUp()
+        //{
+        //    seeder = new TestSeeder();
+        //}
 
-        [TestMethod()]
-        public void GetSummarizedLogAsStringTest_NoErrors_ShouldReturnNoLogs()
-        {
-            ErrorLogger.GetLogList().Clear();
-            seeder.InitList();
+        //[TestMethod()]
+        //public void GetSummarizedLogAsStringTest_NoErrors_ShouldReturnNoLogs()
+        //{
+        //    ErrorLogger.GetLogList().Clear();
+        //    seeder.InitList();
 
-            const string expected = "No Logs";
-            var actual = ErrorLogger.GetSummarizedLogAsString();
-            Assert.AreEqual(expected, actual);
-        }
+        //    const string expected = "No Logs";
+        //    var actual = ErrorLogger.GetSummarizedLogAsString();
+        //    Assert.AreEqual(expected, actual);
+        //}
 
-        [TestMethod()]
-        public void GetSummarizedLogAsStringTest_UpdateEconomicObjectWithEmptyName_ShouldReturnLogMessageStringWasEmpty()
-        {
-            ErrorLogger.GetLogList().Clear();
-            seeder.InitList();
-            seeder.ecoController.UpdateEconomicObjectName("Rent", "");
+        //[TestMethod()]
+        //public void GetSummarizedLogAsStringTest_UpdateEconomicObjectWithEmptyName_ShouldReturnLogMessageStringWasEmpty()
+        //{
+        //    ErrorLogger.GetLogList().Clear();
+        //    seeder.InitList();
+        //    seeder.ecoController.UpdateEconomicObjectName("Rent", "");
 
-            var expected = "1: " + DateTime.Now + " BudgetCalculator.Controllers.EconomicController String name was empty\n";
-            var actual = ErrorLogger.GetSummarizedLogAsString();
-            Assert.AreEqual(expected, actual);
-        }
+        //    var expected = "1: " + DateTime.Now + " BudgetCalculator.Controllers.EconomicController String name was empty\n";
+        //    var actual = ErrorLogger.GetSummarizedLogAsString();
+        //    Assert.AreEqual(expected, actual);
+        //}
 
-        [TestMethod()]
-        public void AddTest_AddEconomicObjectWithDoubleMaxValue_ShouldReturnAnErrorInLogList()
-        {
-            ErrorLogger.GetLogList().Clear();
-            seeder.InitList();
-            seeder.ecoController.UpdateEconomicObjectAmount("Rent", double.MaxValue);
+        //[TestMethod()]
+        //public void AddTest_AddEconomicObjectWithDoubleMaxValue_ShouldReturnAnErrorInLogList()
+        //{
+        //    ErrorLogger.GetLogList().Clear();
+        //    seeder.InitList();
+        //    seeder.ecoController.UpdateEconomicObjectAmount("Rent", double.MaxValue);
 
-            const string expected = "BudgetCalculator.Controllers.EconomicController Amount was more than double.MaxValue";
-            var actual = ErrorLogger.GetLogList()[0];
-            Assert.AreEqual(expected, actual);
-        }
+        //    const string expected = "BudgetCalculator.Controllers.EconomicController Amount was more than double.MaxValue";
+        //    var actual = ErrorLogger.GetLogList()[0];
+        //    Assert.AreEqual(expected, actual);
+        //}
     }
 }
