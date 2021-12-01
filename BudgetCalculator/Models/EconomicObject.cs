1b811339dc22f385711d7af91325d011cdfb21bf
Emma-LinLin <74680845+Emma-LinLin@users.noreply.github.com>
2021-12-01 13:15:19 +0100

    removed calculator and economicType

--- BudgetCalculator/Models/EconomicObject.cs
+++ BudgetCalculator/Models/EconomicObject.cs
@@ -5,7 +5,6 @@
     /// </summary>
     public class EconomicObject
     {
-        public EconomicType Type { get; set; }
         public string Name { get; set; }
         public double Amount { get; set; }
     }
