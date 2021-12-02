7d71457888cec0fd59e56ee14c43ce288442fa95
Oskar Kling <oskarkling@gmail.com>
2021-12-02 15:23:46 +0100

    add

---
+++ WpfApp1/Backend/Database/MyDBContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Protocols;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BudgetCalculator;
+using System.Configuration;
+
+namespace WpfApp1.Backend
+{
+    public class MyDbContext : DbContext
+    {
+        public readonly string dbConnStr = ConfigurationManager.ConnectionStrings["MyConnectionString"].ConnectionString;
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<Income> Incomes { get; set; }
+        public DbSet<Expense> Expenses { get; set; }
+        public DbSet<Goal> Goals { get; set; }
+        public DbSet<Saving> Savings { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(dbConnStr);
+        }
+    }
+}
