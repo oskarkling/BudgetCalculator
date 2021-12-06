66031c863940d3d512f7fce68cb5e22bf1dbaa23
Emma-LinLin <74680845+Emma-LinLin@users.noreply.github.com>
2021-12-06 13:40:21 +0100

    add skeleton for login/logout methods

--- BudgetCalculator/Controllers/AccountController.cs
+++ BudgetCalculator/Controllers/AccountController.cs
@@ -1,12 +1,53 @@
 using System;
+using System.Collections.Generic;
+
 namespace BudgetCalculator
 {
     public class AccountController
     {
-        public Account CurrentAccount { get; set; } 
-        public bool AccountLoggedIn {get; set;}
+        public Account CurrentAccount { get; set; }
+        public bool AccountLoggedIn { get; set; }
         public EconomicController ecoController { get; set; }
 
-        
+        public List<Expense> listOfExpenses = new();
+        public List<Income> listOfIncomes = new();
+        public List<Saving> listOfSavings = new();
+        public List<Goal> listOfGoals = new();
+
+        public void Login(string username, string password)
+        {
+            if (username != string.Empty && password != string.Empty)
+            {
+                if(checkUsernameAndPassword(username, password))
+                {
+                    //print successmessage to UI
+                }
+                else
+                {
+                    //print errormessage to UI
+                }
+            }
+        }
+
+        public void Logout(Account account)
+        {
+            if (AccountLoggedIn)
+            {
+                AccountLoggedIn = false;
+            }
+        }
+
+
+
+        private bool checkUsernameAndPassword(string username, string password)
+        {
+            //try catch
+            //skicka vidare information till CRUD
+            //Får svar kontrollerar nullcheck och sätter respons till CurrentAccount,
+            //sätt AccountLoggedIn = true, retunera AccountLoggedIn
+            return false;
+        }
+
+
     }
 }
