f4c6e1c3af924069801251828033de4a07c0bb1c
Oscar Möller <oscarmoller94@outlook.com>
2021-12-15 12:09:59 +0100

    made changes to the homeview

--- WpfApp1/ViewsModels/MainWindow.xaml.cs
+++ WpfApp1/ViewsModels/MainWindow.xaml.cs
@@ -24,6 +24,7 @@ namespace WpfApp1
         public MainWindow()
         {
             InitializeComponent();
+            GreetUser();
             
         }
         private void openExpenseViewBtn_Click(object sender, RoutedEventArgs e)
@@ -49,6 +50,34 @@ namespace WpfApp1
             AddGoal addGoal = new AddGoal();
             addGoal.Show();
             this.Close();
+        }
+        private void Incomes_Click(object sender, RoutedEventArgs e)
+        {
+            AddIncome income = new AddIncome();
+            income.Show();
+            this.Close();
+        }
+        private void Expenses_Click(object sender, RoutedEventArgs e)
+        {
+            AddExpense expense = new AddExpense();
+            expense.Show();
+            this.Close();
+        }
+        private void Savings_Click(object sender, RoutedEventArgs e)
+        {
+            AddSaving saving = new AddSaving();
+            saving.Show();
+            this.Close();
+        }
+        private void Goals_Click(object sender, RoutedEventArgs e)
+        {
+            AddGoal goal = new AddGoal();
+            goal.Show();
+            this.Close();
+        }
+        private void GreetUser()
+        {
+            greetUser.Text = $"Welcome {BackendManager.accountController.CurrentAccount.Username}";
         }
     }
 }
